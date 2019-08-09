12dc916fb0e1ae944eabbbe710c0ebf0af54d015
Rayffer <oscar.guillamon@gmail.com>
2019-08-09 10:09:58 +0200

    1. Rayffer.PersonalPortfolio.ConjectureCalculators initial commit 2. Rayffer.PersonalPortfolio.Sorters Initial commit

---
+++ C#/Rayffer.PersonalPortfolio/Rayffer.PersonalPortfolio.ConjectureCalculators/CollatzConjecture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rayffer.PersonalPortfolio.ConjectureCalculators
+{
+    public static class CollatzConjecture
+    {
+        public static IEnumerable<long> CalculateSteps(long startNumber)
+        {
+            if (startNumber < 0)
+                return new List<long>();
+
+            long conjectureValue = startNumber;
+
+            List<long> conjectureSteps = new List<long>();
+
+            Task conjectureTask = Task.Run(() =>
+            {
+                while (conjectureValue > 1)
+                {
+                    conjectureSteps.Add(conjectureValue);
+                    conjectureValue = conjectureValue % 2 == 0 ? conjectureValue / 2 : conjectureValue * 3;
+                }
+            });
+
+            if (Task.WaitAll(new Task[] { conjectureTask }, new TimeSpan(0, 5, 0)))
+            {
+                return conjectureSteps;
+            }
+            else
+            {
+                return new List<long>();
+            }
+
+        }
+    }
+}
--- C#/Rayffer.PersonalPortfolio/Rayffer.PersonalPortfolio.TestLaboratory/Program.cs
+++ C#/Rayffer.PersonalPortfolio/Rayffer.PersonalPortfolio.TestLaboratory/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Rayffer.PersonalPortfolio.ConjectureCalculators;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Rayffer.PersonalPortfolio.TestLaboratory
@@ -6,7 +9,19 @@ namespace Rayffer.PersonalPortfolio.TestLaboratory
     internal class Program
     {
         private static void Main(string[] args)
+
         {
+            Stopwatch conjectureStopWatch = new Stopwatch();
+            for (int conjcetureStartingNumber = 0; conjcetureStartingNumber < 1000000; conjcetureStartingNumber++)
+            {
+                conjectureStopWatch.Restart();
+                IEnumerable<long> conjectureSteps = CollatzConjecture.CalculateSteps(conjcetureStartingNumber);
+                conjectureStopWatch.Stop();
+
+                Console.WriteLine($"The collatz conjecture for {conjcetureStartingNumber} value took {conjectureStopWatch.Elapsed.TotalSeconds} seconds and {conjectureSteps.Count()} steps");
+            }
+
+            Console.ReadKey();
         }
     }
 }
