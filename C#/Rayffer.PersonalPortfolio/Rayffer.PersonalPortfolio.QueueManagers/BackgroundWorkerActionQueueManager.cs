aafb718e075511f79fe5b90416ec6f85a710303e
Rayffer <oscar.guillamon@gmail.com>
2019-09-12 09:16:32 +0200

    1. Document BackgroundWorkerActionQueueManager

--- C#/Rayffer.PersonalPortfolio/Rayffer.PersonalPortfolio.QueueManagers/BackgroundWorkerActionQueueManager.cs
+++ C#/Rayffer.PersonalPortfolio/Rayffer.PersonalPortfolio.QueueManagers/BackgroundWorkerActionQueueManager.cs
@@ -5,13 +5,18 @@ using System.Threading;
 
 namespace Rayffer.PersonalPortfolio.QueueManagers
 {
+    /// <summary>
+    /// This is a class that performs task asynchronously in a fire and forget fashion, the ActionQueue is processed as a FIFO queue.
+    /// The user does not know when the action will complete, only that it will if no exceptions are thrown.
+    /// </summary>
     public class BackgroundWorkerActionQueueManager : IDisposable
     {
         public bool IsBusy { get; private set; }
 
-        private BlockingCollection<Action> actionQueue;
+        private readonly BlockingCollection<Action> actionQueue;
         private readonly BackgroundWorker backgroundWorker;
-        private CancellationTokenSource cancellationTokenSource;
+        private readonly CancellationTokenSource cancellationTokenSource;
+        public RunWorkerCompletedEventHandler runWorkerCompletedEventHandler;
 
         public BackgroundWorkerActionQueueManager()
         {
@@ -20,14 +25,41 @@ namespace Rayffer.PersonalPortfolio.QueueManagers
             cancellationTokenSource = new CancellationTokenSource();
             backgroundWorker.WorkerSupportsCancellation = true;
             backgroundWorker.DoWork += BackgroundWorker_ManageQueue;
-            backgroundWorker.RunWorkerAsync();
         }
 
+        /// <summary>
+        /// In case the user desires to recieve an event when an exception is thrown to manage it or the backgroundworker finishes its tasks.
+        /// </summary>
+        /// <param name="runWorkerCompletedEventHandler">
+        /// The method that will handle the completion of the background worker work or exception that has been thrown
+        /// </param>
+        public BackgroundWorkerActionQueueManager(RunWorkerCompletedEventHandler runWorkerCompletedEventHandler) : base()
+        {
+            this.runWorkerCompletedEventHandler = runWorkerCompletedEventHandler;
+            backgroundWorker.RunWorkerCompleted += runWorkerCompletedEventHandler;
+        }
+
+        /// <summary>
+        /// This methods enqueues an action to be performed by the backgroundworker in the class's queue, the action can be anything that the user
+        /// can come up with, mind that the user has to control the exceptions that are thrown.
+        /// </summary>
+        /// <param name="actionToEnqueue">
+        /// The action to enqueue
+        /// </param>
         public void EnqueueAction(Action actionToEnqueue)
         {
             actionQueue.Add(actionToEnqueue);
+            if (!backgroundWorker.IsBusy)
+                backgroundWorker.RunWorkerAsync();
         }
 
+        /// <summary>
+        /// This method manages the queue, taking actions from the queue and performing them. It won't process the next action (if any) until the current
+        /// one has finished. Once it finishes an action, it will inmediately take another one if the queue still holds items or wait for the following
+        /// action to be enqueued.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void BackgroundWorker_ManageQueue(object sender, DoWorkEventArgs e)
         {
             while (!backgroundWorker.CancellationPending)
@@ -63,6 +95,11 @@ namespace Rayffer.PersonalPortfolio.QueueManagers
                     backgroundWorker.CancelAsync();
                     cancellationTokenSource.Cancel();
                     backgroundWorker.DoWork -= BackgroundWorker_ManageQueue;
+                    if (runWorkerCompletedEventHandler != null)
+                    {
+                        backgroundWorker.RunWorkerCompleted -= runWorkerCompletedEventHandler;
+                        runWorkerCompletedEventHandler = null;
+                    }
                     cancellationTokenSource.Dispose();
                     backgroundWorker.Dispose();
                 }
