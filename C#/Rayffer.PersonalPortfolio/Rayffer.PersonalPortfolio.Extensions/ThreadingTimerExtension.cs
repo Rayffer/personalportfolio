00d1f7ff8af3b17fb2df0d69266acc3cdb07ec44
Rayffer <oscar.guillamon@gmail.com>
2019-10-08 13:41:08 +0200

    1. Add extensions for the Timer in System.Threading namespace to ease its usage.

--- C#/Rayffer.PersonalPortfolio/Rayffer.PersonalPortfolio.Extensions/ThreadingTimerExtension.cs
+++ C#/Rayffer.PersonalPortfolio/Rayffer.PersonalPortfolio.Extensions/ThreadingTimerExtension.cs
@@ -1,18 +1,18 @@
+using System;
 
-
-    public static class TimerExtensions
+public static class TimerExtensions
+{
+    public static void StopTimer(this System.Threading.Timer timer)
     {
-        public static void StopTimer(this System.Threading.Timer timer)
-        {
-            timer.Change(
-                System.Threading.Timeout.Infinite,
-                System.Threading.Timeout.Infinite);
-        }
-
-        public static void SetNextExecution(this System.Threading.Timer timer, TimeSpan executionTime)
-        {
-            timer.Change(
-                executionTime.Milliseconds,
-                System.Threading.Timeout.Infinite);
-        }
+        timer.Change(
+            System.Threading.Timeout.Infinite,
+            System.Threading.Timeout.Infinite);
     }
+
+    public static void SetNextExecution(this System.Threading.Timer timer, TimeSpan executionTime)
+    {
+        timer.Change(
+            executionTime.Milliseconds,
+            System.Threading.Timeout.Infinite);
+    }
+}
