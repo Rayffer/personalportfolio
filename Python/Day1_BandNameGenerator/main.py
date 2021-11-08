b78484c39f5bd46a2db27269eb6439f569d68128
Rayffer <oscar.guillamon@gmail.com>
2021-11-08 23:06:11 +0100

    Day 1 of 100 of Python Code

---
+++ Python/Day1_BandNameGenerator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+town_name = input("What is your town name?")
+animal_name = input("What is your favorite animal?")
+print(f"Your band name could be {town_name}'s {animal_name}")
