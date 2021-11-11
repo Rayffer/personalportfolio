5363094bb82d8fd67504a0ac3abbcb4bbe53ccad
Rayffer <oscar.guillamon@gmail.com>
2021-11-11 20:17:26 +0100

    BMI 2.0 exercise

---
+++ Python/Day 3/BMI 2.0/main.py
@@ -0,0 +1,26 @@
+def convert_to_float(input):
+    try:
+        return float(input)
+    except ValueError:
+        print("Please enter a float value")
+        return ""
+
+height = ""
+while(height == ""):
+    height = convert_to_float(input("Enter your height in m:"))
+weight = ""
+while(weight == ""):
+    weight = convert_to_float(input("Enter your weight in kg:"))
+bmi = round(weight / height ** 2, 2)
+print(f"Your bmi is: {bmi}")
+
+if (bmi < 18.5):
+    print("You are underweight")
+elif (bmi >= 18.5 and bmi < 25):
+    print("You are normal weighted")
+elif (bmi >= 25 and bmi < 30):
+    print("You are overweight")
+elif (bmi >= 30 and bmi < 35):
+    print("You are obese")
+elif (bmi >= 35):
+    print("You are morbidly obese")
