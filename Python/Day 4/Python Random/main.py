b269b31c9d4cf13b99ee2d8c0d3f4724ab4ec5d2
Rayffer <oscar.guillamon@gmail.com>
2021-11-16 19:06:09 +0100

    Python random exploration

---
+++ Python/Day 4/Python Random/main.py
@@ -0,0 +1,10 @@
+import random
+
+random_integer = random.randint(1, 10)
+print(f"Your random int number is: {random_integer}")
+
+random_float = random.random()
+print(f"Your random float number between 0 and 1 is: {random_float}")
+
+random_float_2 = random.random() * 5
+print(f"Your random number between 0 and 5 is {random_float_2}")
