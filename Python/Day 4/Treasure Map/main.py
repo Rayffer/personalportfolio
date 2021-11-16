1fad084e33c765ddcad8d4c8a4819e02d044a359
Rayffer <oscar.guillamon@gmail.com>
2021-11-16 20:54:16 +0100

    Day 4 - Treasure map exercise

---
+++ Python/Day 4/Treasure Map/main.py
@@ -0,0 +1,14 @@
+print("Welcome to the Treasure Map")
+
+row_1 = ["🗌", "🗌", "🗌"]
+row_2 = ["🗌", "🗌", "🗌"]
+row_3 = ["🗌", "🗌", "🗌"]
+
+map = [ row_1, row_2, row_3 ]
+print(f"{row_1}\n{row_2}\n{row_3}")
+
+position = input("Where do you want to put the treasure? (Enter X and Y coordinates):")
+
+map[int(position[1])][int(position[0])] = "❌"
+
+print(f"{row_1}\n{row_2}\n{row_3}")
