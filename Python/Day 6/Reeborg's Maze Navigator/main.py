41da960244ee81194489940f0e3f58e8e16c6567
Rayffer <oscar.guillamon@gmail.com>
2021-11-30 21:22:27 +0100

    handle edge case in Maze Navigator where Reeborg's could fall in a loop

--- Python/Day 6/Reeborg's Maze Navigator/main.py
+++ Python/Day 6/Reeborg's Maze Navigator/main.py
@@ -2,12 +2,20 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
+
+right_turns = 0
     
 while not at_goal():
-    if (right_is_clear()):
+    if (right_turns > 4):
+        turn_left()
+        right_turns = 0
+    elif (right_is_clear()):
         turn_right()
         move()
+        right_turns += 1
     elif (front_is_clear()):
         move()
+        right_turns = 0
     else:
         turn_left()
+        right_turns = 0
