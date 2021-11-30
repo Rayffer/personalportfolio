2bef0d292983496fb2a23e03bd936bcc2a385dc5
Rayffer <oscar.guillamon@gmail.com>
2021-11-30 20:12:41 +0100

    Reeborg's Hurdle Challengue

---
+++ Python/Day 6/Reeborg's Hurdles Challengue/main.py
@@ -0,0 +1,26 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+   
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
