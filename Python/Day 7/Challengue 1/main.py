3148f5774a2c24b8a15809e5d5401c94d480f407
Rayffer <oscar.guillamon@gmail.com>
2021-12-02 19:07:56 +0100

    Python Day 7 of 100 Challengue 1 exercise

---
+++ Python/Day 7/Challengue 1/main.py
@@ -0,0 +1,11 @@
+import random
+import re
+word_list = ["ardvarkl", "baboon", "camel"]
+
+word_to_guess = random.choice(word_list)
+
+print("Welcome to the hangman game!")
+guess = input("Please input a letter to guess the hangman word: ")[0].lower()
+
+for letter in word_to_guess:
+    print("Correct guess" if guess == letter else "Incorrect guess")
