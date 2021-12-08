66f986f2d16f4d9129a003e49374844ac2cfdd1a
Rayffer <oscar.guillamon@gmail.com>
2021-12-08 16:59:15 +0100

    Add missing space after input function

--- Python/Day 8/Paint Area Calculator/main.py
+++ Python/Day 8/Paint Area Calculator/main.py
@@ -15,11 +15,11 @@ width = ""
 height = ""
 coverage_per_can = ""
 while width == "":
-    width = convert_to_float(input("How wide is the wall?"))
+    width = convert_to_float(input("How wide is the wall? "))
 while height == "":
-    height = convert_to_float(input("How tall is the wall?"))
+    height = convert_to_float(input("How tall is the wall? "))
 while coverage_per_can == "":
-    coverage_per_can = convert_to_float(input("How much area does each can cover?"))
+    coverage_per_can = convert_to_float(input("How much area does each can cover? "))
 
 amount_of_cans = calculate_required_paint_cans(width, height, coverage_per_can)
 
