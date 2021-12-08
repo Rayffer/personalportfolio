dcbb7c2ee10427a306e7d484a8d5c698719250f2
Rayffer <oscar.guillamon@gmail.com>
2021-12-08 18:02:28 +0100

    Python Day 8 of 100: Caesar Cypher project refactor

--- Python/Day 8/Caesar Cypher/main.py
+++ Python/Day 8/Caesar Cypher/main.py
@@ -1,16 +1,11 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt(text: str, shift: int):
-    encoded_text = ""
-    for letter in text:
-        encoded_text += alphabet[(alphabet.index(letter) + shift) % len(alphabet)]
-    print(f"The encoded text is {encoded_text}")
-
-def decrypt(text: str, shift: int):
-    encoded_text = ""
-    for letter in text:
-        encoded_text += alphabet[(alphabet.index(letter) - shift) % len(alphabet)]
-    print(f"The encoded text is {encoded_text}")
+def caesar(text_to_process: str, shift: int, direction:str):
+    result_text = ""
+    shift_to_apply = (shift if direction == "encode" else -shift)
+    for letter in text_to_process:
+        result_text += alphabet[(alphabet.index(letter) + shift_to_apply) % len(alphabet)]
+    print(f'The {direction}d text is {result_text}')
 
 print("Welcome to the Caesar Cypher")
 
@@ -18,7 +13,4 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if (direction == "encode"):
-    encrypt(text, shift)
-elif (direction == "decode"):
-    decrypt(text, shift)
+caesar(text, shift, direction)
